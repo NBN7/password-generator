45e2a8377696cab7fa1b22c3dea14c854a434c0c
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-09-30 20:41:57 -0300

    add user card

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,22 @@
+import { Divider } from "@nextui-org/react";
+
+import { PasswordGenerator } from "./PasswordGenerator";
+import { PasswordInput } from "./PasswordInput";
+
+export const UserCard = () => {
+  return (
+    <main className="select-none w-full sm:h-screen flex justify-center p-4">
+      <div className="w-full sm:w-[400px] flex flex-col sm:justify-center gap-2 p-4">
+        <h2 className="text-2xl">Password Generator</h2>
+
+        <Divider className="my-4" />
+
+        <PasswordInput />
+
+        <Divider className="my-4" />
+
+        <PasswordGenerator />
+      </div>
+    </main>
+  );
+};
