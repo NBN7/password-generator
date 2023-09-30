8e4d8d885b18a7fae563e8212efaa966fe2d0279
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-09-30 20:48:26 -0300

    update ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { NextUIProvider } from "@nextui-org/react";
+
+import { DataContextProvider } from "./context/dataContext";
+
+import { UserCard } from "./components/UserCard";
+
 function App() {
-  return <></>;
+  return (
+    <NextUIProvider className="dark">
+      <DataContextProvider>
+        <UserCard />
+      </DataContextProvider>
+    </NextUIProvider>
+  );
 }
 
 export default App;
