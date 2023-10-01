3f131ac9e199e472e9b8b0ca78c83bf9f18a95c5
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-01 20:04:46 -0300

    update user card | add hot toast

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -6,7 +6,7 @@ import { PasswordInput } from "./PasswordInput";
 export const UserCard = () => {
   return (
     <main className="select-none w-full sm:h-screen flex justify-center p-4">
-      <div className="w-full sm:w-[400px] flex flex-col sm:justify-center gap-2 p-4">
+      <section className="w-full sm:w-[400px] flex flex-col sm:justify-center gap-2 p-4">
         <h2 className="text-2xl">Password Generator</h2>
 
         <Divider className="my-4" />
@@ -16,7 +16,7 @@ export const UserCard = () => {
         <Divider className="my-4" />
 
         <PasswordGenerator />
-      </div>
+      </section>
     </main>
   );
 };
