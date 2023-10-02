63fd822d0ecdd2013d0f2d44bb28a905e4442d9f
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-01 22:24:16 -0300

    update notifications function

--- src/utils/toastNotifications.ts
+++ src/utils/toastNotifications.ts
@@ -1,23 +1,18 @@
 import { toast } from "react-hot-toast";
 
+const toastStyle = {
+  style: {
+    backgroundColor: "#006FEF",
+    color: "#fff",
+    borderRadius: "10px",
+  },
+  duration: 1500,
+};
+
 export const toastSuccess = (message: string) => {
-  toast.success(message, {
-    style: {
-      backgroundColor: "#006FEF",
-      color: "#fff",
-      borderRadius: "10px",
-    },
-    duration: 1500,
-  });
+  toast.success(message, toastStyle);
 };
 
 export const toastError = (message: string) => {
-  toast.error(message, {
-    style: {
-      backgroundColor: "#006FEF",
-      color: "#fff",
-      borderRadius: "10px",
-    },
-    duration: 1500,
-  });
+  toast.error(message, toastStyle);
 };
